fa481bd72d23f53f26376277f772eb34d87233b4
Aastha Sinha <asthasinha2305@gmail.com>
2022-12-20 15:29:12 +0530

    basic structure done

---
+++ function.js
@@ -0,0 +1,28 @@
+var input = document.querySelector('#zipcode');
+var city = document.querySelector('#city');
+var state = document.querySelector('#state');
+var country = document.querySelector('#country');
+var info = document.querySelector('.info');
+var codelength = /^\d{6}$/
+
+input.addEventListener('keyup', () => {
+    if (input.value.match(codelength)) {
+        $.ajax({
+            type: "GET",
+            url: "https://api.postalpincode.in/pincode/" + input.value,
+
+            success: (response) => {
+                // info.innerHTML = (response[0].PostOffice[0].Name);
+                city.value = (response[0].PostOffice[0].District);
+                state.value = (response[0].PostOffice[0].State);
+                country.value = (response[0].PostOffice[0].Country);
+            }
+        });
+        info.innerHTML = "";
+
+    }
+    else{
+        info.innerHTML = "Type valid zipcode";
+    }
+
+}); 
